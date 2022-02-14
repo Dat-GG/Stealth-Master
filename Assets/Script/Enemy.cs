feec11246fac9ae5f645465e85a9df32017f1c55
Le Thanh Dat <lethanhdat160796@gmail.com>
2022-02-14 17:09:42 +0700

    EnemyState

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    EnemyStates _currentstate;
     void Start()
     {
-        
+        _currentstate = GetInitState();
+        if (_currentstate != null)
+            _currentstate.EnterState();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_currentstate != null)
+            _currentstate.UpdateState();
+    }
+    protected virtual EnemyStates GetInitState()
+    {
+        return null;
+    }
+    public void ChangeState(EnemyStates newState)
+    {
+        _currentstate.ExitState();
+        _currentstate = newState;
+        _currentstate.EnterState();
     }
 }
---
+++ Assets/Script/EnemyStates.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStates : MonoBehaviour
+{
+   public virtual void EnterState()
+    {
+
+    }
+
+    public virtual void UpdateState()
+    {
+
+    }
+
+    public virtual void ExitState()
+    {
+
+    }
+}
